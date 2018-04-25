f3c81227768b7a48db222360da7236d76041a816
Moses <maadeagbo@gmail.com>
2018-04-25 08:00:39 -0500

    Updated shader enums script

--- gen_shader_enums.sh
+++ gen_shader_enums.sh
@@ -6,3 +6,8 @@ SDIR=$1
 ./../../bin/dd_shader_reflect -o svis_shader_enums.h -e RE_LineDot \
 	-v "$SDIR/LineDot_V.vert" \
 	-f "$SDIR/LineDot_F.frag"
+
+./../../bin/dd_shader_reflect -o svis_shader_enums.h -a -e RE_Point \
+	-v "$SDIR/PointRend_V.vert" \
+	-g "$SDIR/PointRend_G.geom" \
+	-f "$SDIR/PointRend_F.frag"

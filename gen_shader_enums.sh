6929a9059863db47e6740d7fa35c422d8f9b8b01
Moses <maadeagbo@gmail.com>
2018-05-07 01:22:38 -0500

    Fixed issues in script execution

--- gen_shader_enums.sh
+++ gen_shader_enums.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # render shader
-./../../bin/dd_shader_reflect -o svis_shader_enums.h -e RE_LineDot \
+./../../bin/shader_reflect -o svis_shader_enums.h -e RE_LineDot \
 	-v "./LineDot_V.vert" \
 	-f "./LineDot_F.frag"
 
-./../../bin/dd_shader_reflect -o svis_shader_enums.h -a -e RE_Point \
+./../../bin/shader_reflect -o svis_shader_enums.h -a -e RE_Point \
 	-v "./PointRend_V.vert" \
 	-g "./PointRend_G.geom" \
 	-f "./PointRend_F.frag"
